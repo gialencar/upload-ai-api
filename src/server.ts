9e8975664a7dacf53f3d7ceedf63d217f99966f7
Gilson Alencar <gilson.alencar94@gmail.com>
2023-09-13 01:01:50 -0300

    feat: create `generate-ai-completions` route

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
-import fastify from 'fastify';
+import { fastify } from 'fastify';
+import { fastifyCors } from '@fastify/cors';
 import { getAllPromptsRoute } from './routes/get-all-prompts';
 import { uploadVideoRoute } from './routes/upload-video';
 import { createTranscriptionRoute } from './routes/create-transcription';
+import { generateAICompletionRoute } from './routes/generate-ai-completion';
 
 const PORT = Number(process.env.PORT) || 3333;
 const app = fastify({ logger: true });
 
+app.register(fastifyCors, {
+  origin: '*', // TODO - change this to the frontend URL
+});
+
 app.register(getAllPromptsRoute);
 app.register(uploadVideoRoute);
 app.register(createTranscriptionRoute);
+app.register(generateAICompletionRoute);
 
 app.listen({ port: PORT });
